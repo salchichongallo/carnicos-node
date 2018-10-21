22bf245c69df7d49ce792909b7994a57b74a39c3
Jimmy Murillo <salchichongallo1997@gmail.com>
2018-10-21 15:07:21 -0500

    feat(ui): add Card atom

---
+++ packages/ui/atoms/Card.tsx
@@ -0,0 +1,8 @@
+import styled from '@carnicos/ui/styled';
+
+const Card = styled.div`
+  border-radius: ${props => props.theme.borderRadius};
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.38);
+`;
+
+export default Card;
--- packages/ui/atoms/index.ts
+++ packages/ui/atoms/index.ts
@@ -3,3 +3,5 @@ export { default as Logo } from './Logo';
 export { default as Button } from './Button';
 
 export { Title } from './Text';
+
+export { default as Card } from './Card';
