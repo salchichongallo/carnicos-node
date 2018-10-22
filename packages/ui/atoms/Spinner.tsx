99b33aaf76269cfbd6db4cf32b89540f2c4b6857
Jimmy Murillo <salchichongallo1997@gmail.com>
2018-10-22 11:35:13 -0500

    feat(ui): add Spinner atom

---
+++ packages/ui/atoms/Spinner.tsx
@@ -0,0 +1,42 @@
+import styled, { keyframes } from '@carnicos/ui/styled';
+
+type Props = {
+  size?: number;
+  color?: string;
+};
+
+const spin = keyframes`
+  to { transform: rotate(360deg); }
+`;
+
+const calcSize = ({ size }: { size?: number }) => (size ? `${size}px` : '24px');
+
+const Spinner = styled.span<Props>`
+  display: inline-block;
+
+  width: ${calcSize};
+  height: ${calcSize};
+
+  &::before {
+    position: relative;
+
+    display: inline-block;
+
+    box-sizing: border-box;
+
+    width: ${calcSize};
+    height: ${calcSize};
+
+    content: '';
+    animation: ${spin} 2s linear infinite;
+
+    color: ${props => (props.color ? props.color : props.theme.primary)};
+    border: 2px solid currentColor;
+    border-top-color: transparent;
+    border-right-color: currentColor;
+    border-bottom-color: transparent;
+    border-radius: 50%;
+  }
+`;
+
+export default Spinner;
--- packages/ui/atoms/index.ts
+++ packages/ui/atoms/index.ts
@@ -11,3 +11,5 @@ export { default as A } from './A';
 export { default as Link } from './Link';
 
 export { default as Input } from './Input';
+
+export { default as Spinner } from './Spinner';
